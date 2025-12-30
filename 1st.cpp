476a656da307f1301bad3e9bd6ab1ba36830b53b
Mashfik-007 <mashfik-007@192.168.0.105>
2025-12-30 07:54:42 +0600

    1st added

---
+++ 1st.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[] = {1, 212, 323, 454};
+
+    for (auto var : array) {
+        cout << var << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
